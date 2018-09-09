f2a2793c5169418e29b07e3b16503e28e5aafa3f
Gabriel Minucci <garomi173@gmail.com>
2018-09-08 22:41:13 -0300

    Initial Commit

---
+++ TMDb ChallengeTests/TMDb_ChallengeTests.swift
@@ -0,0 +1,36 @@
+//
+//  TMDb_ChallengeTests.swift
+//  TMDb ChallengeTests
+//
+//  Created by Gabriel Minucci on 08/09/18.
+//  Copyright © 2018 Minucci. All rights reserved.
+//
+
+import XCTest
+@testable import TMDb_Challenge
+
+class TMDb_ChallengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
