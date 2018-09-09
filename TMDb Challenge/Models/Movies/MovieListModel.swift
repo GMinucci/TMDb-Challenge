c03ab91daa7ebe1994f88b3ae02584a059901030
Gabriel Minucci <garomi173@gmail.com>
2018-09-08 23:47:59 -0300

    Add movies list model

---
+++ TMDb Challenge/Models/Movies/MovieListModel.swift
@@ -0,0 +1,25 @@
+//
+//  MovieListModel.swift
+//  TMDb Challenge
+//
+//  Created by Gabriel Minucci on 08/09/18.
+//  Copyright © 2018 Minucci. All rights reserved.
+//
+
+import Foundation
+
+struct MovieListModel {
+    let id: Int
+    let voteCount: Int
+    let video: Bool
+    let voteAverage: Float
+    let title: String
+    let popularity: Float
+    let posterPath: String?
+    let originalLanguage: String
+    let originalTitle: String
+    let genreIds: [Int]
+    let backdropPath: String?
+    let adult: Bool
+    let overview: String
+}
