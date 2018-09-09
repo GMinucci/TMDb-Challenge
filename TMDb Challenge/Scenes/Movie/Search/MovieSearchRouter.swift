cb92abfcc310503a799488aef07c76f8fdec46a2
Gabriel Minucci <garomi173@gmail.com>
2018-09-09 19:32:42 -0300

    Add methods to open movie detail from search

--- TMDb Challenge/Scenes/Movie/Search/MovieSearchRouter.swift
+++ TMDb Challenge/Scenes/Movie/Search/MovieSearchRouter.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @objc protocol MovieSearchRoutingLogic {
-
+    func routeToMovieDetail(segue: UIStoryboardSegue?)
 }
 
 protocol MovieSearchDataPassing {
@@ -21,5 +21,19 @@ class MovieSearchRouter: NSObject, MovieSearchRoutingLogic, MovieSearchDataPassi
     // Var's
     weak var viewController: MovieSearchViewController?
     var dataStore: MovieSearchDataStore?
+    
+    func routeToMovieDetail(segue: UIStoryboardSegue?) {
+        if var destinationDS = (segue?.destination as? MovieDetailViewController)?.router?.dataStore {
+            passDataToMovieDetail(source: dataStore, destination: &destinationDS)
+        }
+    }
+    
+    // MARK: Pass data
+    func passDataToMovieDetail(source: MovieSearchDataStore?, destination: inout MovieDetailDataStore) {
+        guard let selectedRow = viewController?.tableView.indexPathForSelectedRow?.row else { return }
+        guard source?.movieList.count ?? 0 > selectedRow else { return }
+        let selectedMovie = source?.movieList[selectedRow]
+        destination.movieID = selectedMovie?.id
+    }
 
 }
